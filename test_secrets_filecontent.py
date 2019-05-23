ec57a2f071eb1493ac0bac8ac6195ff1fbae62f3
ChristyHD <44365312+ChristyHD@users.noreply.github.com>
2019-05-23 17:07:52 +0100

    adding tests

---
+++ test_secrets_filecontent.py
@@ -0,0 +1,260 @@
+import pytest
+import secrets_filecontent
+import re
+
+
+'''All comments at beginning of test sets correspond to id of leak detection rules per
+https://github.com/hmrc/app-config-base/blob/master/leak-detection.conf'''
+
+
+# aws_2
+
+def test_aws_secret_key_matches_with_colon_separator():
+    aws_secret_key = "aws_secret_access_key:H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) == "aws_2"
+
+
+def test_aws_secret_key__does_not_match_with_equals_sign_separator__TO_REVIEW():
+    aws_secret_key = "aws_secret_access_key=H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) is None
+
+
+def test_aws_secret_key_matches_with_whitespace_and_colon_separator():
+    aws_secret_key = "aws_secret_access_key :   H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) == "aws_2"
+
+
+def test_aws_secret_key_matches_with_whitespace_and_equals_sign_separator():
+    aws_secret_key = "aws_secret_access_key =   H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) == "aws_2"
+
+
+def test_aws_secret_key_does_not_match_40_char_base64_string():
+    aws_secret_key = "H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) is None
+
+
+def test_aws_secret_key_does_not_match_more_than_40_characters_in_key():
+    aws_secret_key = "aws_secret_access_key = X12345MEuati12345ed+voyz/UeJ560Bu0cgJcaac"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) is None
+
+
+def test_aws_secret_key_does_not_match_fewer_than_40_characters_in_key():
+    aws_secret_key = "aws_secret_access_key = X12345MEuati12345ed+voyz/UeJ560cgJcaac"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) is None
+
+
+def test_aws_secret_key_does_not_match_with_non_base64_characters():
+    aws_secret_key = "aws_secret_access_key = H5xnFhnR3H/o6nrcfoMLR9VfOl*OY17pa/+PchnA"
+    assert secrets_filecontent.detect_secret_in_line(aws_secret_key) is None
+
+
+# cert_1
+
+def test_cert_1_RSA_header_is_match():
+    cert_1 = "-----BEGIN RSA PRIVATE KEY-----"
+    assert secrets_filecontent.detect_secret_in_line(cert_1) == "cert_1"
+
+
+def test_cert_1_RSA_footer_is_match():
+    cert_1 = "-----END RSA PRIVATE KEY-----"
+    assert secrets_filecontent.detect_secret_in_line(cert_1) == "cert_1"
+
+
+def test_cert_1_DSA_header_is_match():
+    cert_1 = "-----BEGIN DSA PRIVATE KEY----- "
+    assert secrets_filecontent.detect_secret_in_line(cert_1) == "cert_1"
+
+
+def test_cert_1_DSA_footer_is_match():
+    cert_1 = "-----END DSA PRIVATE KEY-----"
+    assert secrets_filecontent.detect_secret_in_line(cert_1) == "cert_1"
+
+
+def test_cert_1_RSA_full_match():
+    cert_1 = "-----BEGIN RSA PRIVATE KEY----- keycontent -----END RSA PRIVATE KEY-----"
+    assert secrets_filecontent.detect_secret_in_line(cert_1) == "cert_1"
+
+
+def test_cert_1_DSA_full_match():
+    cert_1 = "-----BEGIN DSA PRIVATE KEY----- key content -----END RSA PRIVATE KEY-----"
+    assert secrets_filecontent.detect_secret_in_line(cert_1) == "cert_1"
+
+
+def test_cert_1_RSA_pub_key_does_not_match():
+    cert_1 = "ssh-rsa public key content"
+    assert secrets_filecontent.detect_secret_in_line(cert_1) is None
+
+
+# application_secret
+
+def test_application_secret_matches_with_colon_separator():
+    application_secret = "application.secret:\"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(application_secret) == "application_secret"
+
+
+def test_application_secret_matches_with_equals_sign_separator():
+    application_secret = "application.secret=\"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(application_secret) == "application_secret"
+
+
+def test_application_secret_matches_with_whitespace_and_colon_separator():
+    application_secret = "application.secret : \"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(application_secret) == "application_secret"
+
+
+def test_application_secret_matches_with_whitespace_and_equals_sign_separator():
+    application_secret = "application.secret = \"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(application_secret) == "application_secret"
+
+
+def test_application_secret_doesnt_match_as_words_in_string():
+    application_secret = "application secret."
+    assert secrets_filecontent.detect_secret_in_line(application_secret) is None
+
+
+# play_crypto_secret
+
+def test_play_crypto_secret_matches_with_colon_separator():
+    play_crypto_secret = "play.crypto.secret:\"zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k\""
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) == "play_crypto_secret"
+
+
+def test_play_crypto_secret_matches_with_equals_sign_separator():
+    play_crypto_secret = "play.crypto.secret=\"zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k\""
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) == "play_crypto_secret"
+
+
+def test_play_crypto_secret_matches_with_whitespace_and_colon_separator():
+    play_crypto_secret = "play.crypto.secret :  \"zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k\""
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) == "play_crypto_secret"
+
+
+def test_play_crypto_secret_matches_with_whitespace_and_equals_sign_separator():
+    play_crypto_secret = "play.crypto.secret =  \"zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k\""
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) == "play_crypto_secret"
+
+
+def test_play_crypto_secret_matches_with_default_changeme_value():
+    play_crypto_secret = "play.crypto.secret = \"changeme\""
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) == "play_crypto_secret"
+
+
+def test_play_crypto_secret_doesnt_match_when_encrypted_with_colon_separator():
+    play_crypto_secret = "play.crypto.secret:ENC[GPPencrypted"
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) is None
+
+
+def test_play_crypto_secret_doesnt_match_when_encrypted_with_equals_sign_separator():
+    play_crypto_secret = "play.crypto.secret=ENC[GPPencrypted"
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) is None
+
+
+def test_play_crypto_secret_doesnt_match_when_encrypted_with_whitespace_and_colon_separator():
+    play_crypto_secret = "play.crypto.secret :  ENC[GPPencrypted"
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) is None
+
+
+def test_play_crypto_secret_doesnt_match_when_encrypted_with_whitespace_and_equals_sign_separator():
+    play_crypto_secret = "play.crypto.secret =  ENC[GPPencrypted"
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) is None
+
+
+def test_play_crypto_secret_doesnt_match_as_words_in_string():
+    play_crypto_secret = "play crypto secret."
+    assert secrets_filecontent.detect_secret_in_line(play_crypto_secret) is None
+
+
+# cookie_deviceId_secret
+
+def test_cookie_deviceId_secret_matches_with_colon_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret:\"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) == "cookie_deviceId_secret"
+
+
+def test_cookie_deviceId_secret_matches_with_equals_sign_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret=\"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) == "cookie_deviceId_secret"
+
+
+def test_cookie_deviceId_secret_matches_with_whitespace_and_colon_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret :  \"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) == "cookie_deviceId_secret"
+
+
+def test_cookie_deviceId_secret_matches_with_whitespace_and_equals_sign_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret =  \"helloiamasecret\""
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) == "cookie_deviceId_secret"
+
+
+def test_cookie_deviceId_secret_doesnt_match_when_encrypted_with_colon_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret:ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) is None
+
+
+def test_cookie_deviceId_secret_doesnt_match_when_encrypted_with_equals_sign_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret=ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) is None
+
+
+def test_cookie_deviceId_secret_doesnt_match_when_encrypted_with_whitespace_and_colon_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret :  ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) is None
+
+
+def test_cookie_deviceId_secret_doesnt_match_when_encrypted_with_whitespace_and_equals_sign_separator():
+    cookie_deviceId_secret = "cookie.deviceId.secret =  ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) is None
+
+
+def test_cookie_deviceId_secret_doesnt_match_as_words_in_string():
+    cookie_deviceId_secret = "cookie deviceId secret."
+    assert secrets_filecontent.detect_secret_in_line(cookie_deviceId_secret) is None
+
+
+#sso_encryption_key 
+
+def test_sso_encryption_key_matches_with_colon_separator():
+    sso_encryption_key = "sso.encryption.key:P5xsJ9Nt+quxDKzB4DeLfw=="
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) == "sso_encryption_key"
+
+
+def test_sso_encryption_key_matches_with_equals_sign_separator():
+    sso_encryption_key = "sso.encryption.key=P5xsJ9Nt+quxDKzB4DeLfw=="
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) == "sso_encryption_key"
+
+
+def test_sso_encryption_key_matches_with_whitespace_and_colon_separator():
+    sso_encryption_key = "sso.encryption.key :  P5xsJ9Nt+quxDKzB4DeLfw=="
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) == "sso_encryption_key"
+
+
+def test_sso_encryption_key_matches_with_whitespace_and_equals_sign_separator():
+    sso_encryption_key = "sso.encryption.key =  P5xsJ9Nt+quxDKzB4DeLfw=="
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) == "sso_encryption_key"
+
+
+def test_sso_encryption_key_doesnt_match_when_encrypted_with_colon_separator():
+    sso_encryption_key = "sso.encryption.key:ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) is None
+
+
+def test_sso_encryption_key_doesnt_match_when_encrypted_with_equals_sign_separator():
+    sso_encryption_key = "sso.encryption.key=ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) is None
+
+
+def test_sso_encryption_key_doesnt_match_when_encrypted_with_whitespace_and_colon_separator():
+    sso_encryption_key = "sso.encryption.key :  ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) is None
+
+
+def test_sso_encryption_key_doesnt_match_when_encrypted_with_whitespace_and_equals_sign_separator():
+    sso_encryption_key = "sso.encryption.key =  ENC[GPG"
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) is None
+
+
+def test_sso_encryption_key_doesnt_match_as_words_in_string():
+    sso_encryption_key = "sso encryption key."
+    assert secrets_filecontent.detect_secret_in_line(sso_encryption_key) is None
+
---
+++ test_secrets_filename.py
@@ -0,0 +1,238 @@
+import pytest
+import secrets_filename
+import re
+
+'''All comments at beginning of test sets correspond to id of leak detection rules per
+https://github.com/hmrc/app-config-base/blob/master/leak-detection.conf'''
+
+
+# filename_private_key_1
+
+def test_p12_extension_match():
+    filename = "fake.p12"
+    assert secrets_filename.detect_match_against_filename(filename) == "p12"
+
+
+def test_p12_in_filename_does_not_match():
+    filename = "p12.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+# filename_private_key_2
+
+def test_pfx_extension_match():
+    filename = "fake.pfx"
+    assert secrets_filename.detect_match_against_filename(filename) == "pfx"
+
+
+def test_pfx_in_filename_does_not_match():
+    filename = "pfx.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_3
+
+def test_pkcs12_extension_match():
+    filename = "fake.pkcs12"
+    assert secrets_filename.detect_match_against_filename(filename) == "pkcs12"
+
+
+def test_pkcs12_in_filename_does_not_match():
+    filename = "pkcs12.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_5
+
+def test_pem_extension_match():
+    filename = "fake.pem"
+    assert secrets_filename.detect_match_against_filename(filename) == "pem"
+
+
+def test_pem_in_filename_does_not_match():
+    filename = "pem.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_7
+
+def test_rsa_extension_match():
+    filename = "fake_rsa"
+    assert secrets_filename.detect_match_against_filename(filename) == "rsa"
+
+
+def test_rsa_public_key_does_not_match():
+    filename = "fake_rsa.pub"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_8
+
+def test_dsa_extension_match():
+    filename = "fake_dsa"
+    assert secrets_filename.detect_match_against_filename(filename) == "dsa"
+
+
+def test_dsa_public_key_does_not_match():
+    filename = "fake_dsa.pub"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_9
+
+def test_ed25519_extension_match():
+    filename = "fake_ed25519"
+    assert secrets_filename.detect_match_against_filename(filename) == "ed25519"
+
+
+def test_ed25519_public_key_does_not_match():
+    filename = "fake_ed25519.pub"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_10
+
+def test_ecdsa_extension_match():
+    filename = "fake_ecdsa"
+    assert secrets_filename.detect_match_against_filename(filename) == "ecdsa"
+
+
+def test_ecdsa_public_key_does_not_match():
+    filename = "fake_ecdsa.pub"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+# filename_private_key_11
+
+def test_jks_extension_match():
+    filename = "fake.jks"
+    assert secrets_filename.detect_match_against_filename(filename) == "jks"
+
+
+def test_jks_in_filename_does_not_match():
+    filename = "jks.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_bash_config_bashrc_match():
+    filename = ".bashrc"
+    assert secrets_filename.detect_match_against_filename(filename) == "bash/zsh rc file"
+
+
+def test_bashrc_in_filename_does_not_match():
+    filename = ".bashrc.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None 
+
+
+def test_zsh_config_zshrc_match():
+    filename = ".zshrc"
+    assert secrets_filename.detect_match_against_filename(filename) == "bash/zsh rc file"
+
+
+def test_zshrc_in_filename_does_not_match():
+    filename = ".zshrc.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None 
+
+
+def test_bash_config_bash_profile_match():
+    filename = ".bash_profile"
+    assert secrets_filename.detect_match_against_filename(filename) == "bash/zsh profile"
+
+
+def test_bash_profile_in_filename_does_not_match():
+    filename = ".bash_profile.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None 
+
+
+def test_zsh_config_zsh_profile_match():
+    filename = ".zsh_profile"
+    assert secrets_filename.detect_match_against_filename(filename) == "bash/zsh profile"
+
+
+def test_zsh_profile_in_filename_does_not_match():
+    filename = ".zsh_profile.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None 
+
+
+def test_bash_config_bash_aliases_match():
+    filename = ".bash_aliases"
+    assert secrets_filename.detect_match_against_filename(filename) == "bash/zsh aliases file"
+
+
+def test_bash_aliases_in_filename_does_not_match():
+    filename = ".bash_aliases.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None 
+
+
+def test_zsh_config_zsh_aliases_match():
+    filename = ".zsh_aliases"
+    assert secrets_filename.detect_match_against_filename(filename) == "bash/zsh aliases file"
+
+
+def test_zsh_aliases_in_filename_does_not_match():
+    filename = ".zsh_aliases.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None 
+
+
+def test_credential_extension_match():
+    filename = ".credential"
+    assert secrets_filename.detect_match_against_filename(filename) == "credential(s) file"
+
+
+def test_credential_in_filename_does_not_match():
+    filename = "credential.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_credentials_extension_match():
+    filename = ".credentials"
+    assert secrets_filename.detect_match_against_filename(filename) == "credential(s) file"
+
+
+def test_credentials_in_filename_does_not_match():
+    filename = "credentials.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_github_enterprise_extension_match():
+    filename = ".githubenterprise"
+    assert secrets_filename.detect_match_against_filename(filename) == "Github Enterprise file"
+
+
+def test_github_enterprise_in_filename_does_not_match():
+    filename = "githubenterprise.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_apple_keychain_extension_match():
+    filename = ".keychain"
+    assert secrets_filename.detect_match_against_filename(filename) == "Apple Keychain file"
+
+
+def test_apple_keychain_in_filename_does_not_match():
+    filename = "keychain.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_apple_login_keychain_in_filename_does_not_match___TO_REVIEW():
+    filename = "login.keychain"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_keystore_match___TO_REVIEW():
+    filename = "keystore"
+    assert secrets_filename.detect_match_against_filename(filename) == "Keystore/Keyring file"
+
+
+def test_keystore_in_filename_does_not_match():
+    filename = "keystore.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
+
+
+def test_keyring_match___TO_REVIEW():
+    filename = "keyring"
+    assert secrets_filename.detect_match_against_filename(filename) == "Keystore/Keyring file"
+
+
+def test_keyring_in_filename_does_not_match():
+    filename = "keyring.txt"
+    assert secrets_filename.detect_match_against_filename(filename) is None
