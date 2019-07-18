d06d17f5f46624b1f7d32d68ad6a1c514306edcf
ChristyHD <44365312+ChristyHD@users.noreply.github.com>
2019-07-18 14:57:48 +0100

    update tests

--- tests/test_secrets_filecontent.py
+++ tests/test_secrets_filecontent.py
@@ -11,138 +11,218 @@ https://github.com/hmrc/app-config-base/blob/master/leak-detection.conf"""
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "line, file, expected",
     [
-        ("aws_secret_access_key:H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA", "aws_2"),
-        ("aws_secret_access_key :   H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA", "aws_2"),
-        ("aws_secret_access_key =   H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA", "aws_2"),
-        ("aws_secret_access_key=H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA", None),
-        ("H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA", None),
-        ("aws_secret_access_key = X12345MEuati12345ed+voyz/UeJ560Bu0cgJcaac", None),
-        ("aws_secret_access_key = X12345MEuati12345ed+voyz/UeJ560cgJcaac", None),
-        ("aws_secret_access_key = H5xnFhnR3H/o6nrcfoMLR9VfOl*OY17pa/+PchnA", None),
+        (
+            "aws_secret_access_key:H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA",
+            "afile.py",
+            "aws_2",
+        ),
+        (
+            "aws_secret_access_key :   H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA",
+            "afile.txt",
+            "aws_2",
+        ),
+        (
+            "aws_secret_access_key =   H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA",
+            "afile.md",
+            "aws_2",
+        ),
+        (
+            "aws_secret_access_key=H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA",
+            "afile.py",
+            None,
+        ),
+        ("H5xnFhnR3H/o6nrcfoMLR9VfOlfOY17pa/+PchnA", "afile.py", None),
+        (
+            "aws_secret_access_key = X12345MEuati12345ed+voyz/UeJ560Bu0cgJcaac",
+            "afile.py",
+            None,
+        ),
+        (
+            "aws_secret_access_key = X12345MEuati12345ed+voyz/UeJ560cgJcaac",
+            "afile.py",
+            None,
+        ),
+        (
+            "aws_secret_access_key = H5xnFhnR3H/o6nrcfoMLR9VfOl*OY17pa/+PchnA",
+            "afile.py",
+            None,
+        ),
     ],
 )
-def test_aws_2(test_input, expected):
-    assert secrets_filecontent.detect_secret_in_line(test_input) == expected
+def test_aws_2(line, file, expected):
+    assert secrets_filecontent.detect_secret_in_line(line, file) == expected
 
 
 # cert_1
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "line, file, expected",
     [
-        ("-----BEGIN RSA PRIVATE KEY-----", "cert_1"),
-        ("-----END RSA PRIVATE KEY-----", "cert_1"),
-        ("-----BEGIN DSA PRIVATE KEY-----", "cert_1"),
-        ("-----END DSA PRIVATE KEY-----", "cert_1"),
+        ("-----BEGIN RSA PRIVATE KEY-----", "filename.txt", "cert_1"),
+        ("-----END RSA PRIVATE KEY-----", "filename.md", "cert_1"),
+        ("-----BEGIN DSA PRIVATE KEY-----", "filename.py", "cert_1"),
+        ("-----END DSA PRIVATE KEY-----", "filename.js", "cert_1"),
+        ("-----BEGIN RSA PRIVATE KEY-----", "kitchen.yml", None),
+        ("-----BEGIN RSA PRIVATE KEY-----", "kitchen.yaml", None),
         (
             "-----BEGIN RSA PRIVATE KEY----- \n keycontent \n -----END RSA PRIVATE KEY-----",
+            "filename.txt",
             "cert_1",
         ),
         (
             "-----BEGIN DSA PRIVATE KEY----- \n keycontent \n -----END RSA PRIVATE KEY-----",
+            "filename.txt",
             "cert_1",
         ),
-        ("ssh-rsa public key content", None),
-        ("ssh-dsa public key content", None),
+        ("ssh-rsa public key content", "filename.txt", None),
+        ("ssh-dsa public key content", "filename.txt", None),
     ],
 )
-def test_cert_1(test_input, expected):
-    assert secrets_filecontent.detect_secret_in_line(test_input) == expected
+def test_cert_1(line, file, expected):
+    assert secrets_filecontent.detect_secret_in_line(line, file) == expected
 
 
 # application_secret
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "line, file, expected",
     [
-        ('application.secret:"helloiamasecret"', "application_secret"),
-        ('application.secret="helloiamasecret"', "application_secret"),
-        ('application.secret : "helloiamasecret"', "application_secret"),
-        ('application.secret = "helloiamasecret"', "application_secret"),
-        ("application secret", None),
-        ("application.secret", None),
-        ("application_secret", None),
+        ('application.secret:"helloiamasecret"', "filename.txt", "application_secret"),
+        ('application.secret="helloiamasecret"', "filename.txt", "application_secret"),
+        (
+            'application.secret : "helloiamasecret"',
+            "filename.txt",
+            "application_secret",
+        ),
+        (
+            'application.secret = "helloiamasecret"',
+            "filename.txt",
+            "application_secret",
+        ),
+        ('application.secret = "helloiamasecret"', "filename.scala", None),
+        ('application.secret = "helloiamasecret"', " /conf/application.conf", None),
+        ("application secret", "filename.txt", None),
+        ("application.secret", "filename.txt", None),
+        ("application_secret", "filename.txt", None),
     ],
 )
-def test_application_secret(test_input, expected):
-    assert secrets_filecontent.detect_secret_in_line(test_input) == expected
+def test_application_secret(line, file, expected):
+    assert secrets_filecontent.detect_secret_in_line(line, file) == expected
 
 
 # play_crypto_secret
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "line, file, expected",
     [
         (
             'play.crypto.secret:"zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k"',
+            "filename.txt",
             "play_crypto_secret",
         ),
         (
             'play.crypto.secret="zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k"',
+            "filename.txt",
             "play_crypto_secret",
         ),
         (
             'play.crypto.secret :  "zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k"',
+            "filename.txt",
             "play_crypto_secret",
         ),
         (
             'play.crypto.secret =  "zLiPolptzA5HLnRG9XAF6hQGP4QGQvEd82W27dzsI8HpFQRToMD7m8f78LQ0Ur7k"',
+            "filename.txt",
             "play_crypto_secret",
         ),
-        ('play.crypto.secret = "changeme"', "play_crypto_secret"),
-        ("play.crypto.secret:ENC[GPPencrypted", None),
-        ("play.crypto.secret=ENC[GPPencrypted", None),
-        ("play.crypto.secret :  ENC[GPPencrypted", None),
-        ("play.crypto.secret =  ENC[GPPencrypted", None),
-        ("play crypto secret.", None),
+        ('play.crypto.secret = "changeme"', "filename.txt", "play_crypto_secret"),
+        ('play.crypto.secret = "changeme"', "filename.scala", None),
+        ('play.crypto.secret = "changeme"', " /conf/application.conf", None),
+        ("play.crypto.secret:ENC[GPPencrypted", "filename.txt", None),
+        ("play.crypto.secret=ENC[GPPencrypted", "filename.txt", None),
+        ("play.crypto.secret :  ENC[GPPencrypted", "filename.txt", None),
+        ("play.crypto.secret =  ENC[GPPencrypted", "filename.txt", None),
+        ("play crypto secret.", "filename.txt", None),
     ],
 )
-def test_play_crypto_secret(test_input, expected):
-    assert secrets_filecontent.detect_secret_in_line(test_input) == expected
+def test_play_crypto_secret(line, file, expected):
+    assert secrets_filecontent.detect_secret_in_line(line, file) == expected
 
 
 # cookie_deviceId_secret
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "line, file, expected",
     [
-        ('cookie.deviceId.secret:"helloiamasecret"', "cookie_deviceId_secret"),
-        ('cookie.deviceId.secret="helloiamasecret"', "cookie_deviceId_secret"),
-        ('cookie.deviceId.secret :  "helloiamasecret"', "cookie_deviceId_secret"),
-        ('cookie.deviceId.secret =  "helloiamasecret"', "cookie_deviceId_secret"),
-        ("cookie.deviceId.secret:ENC[GPG", None),
-        ("cookie.deviceId.secret=ENC[GPG", None),
-        ("cookie.deviceId.secret :  ENC[GPG", None),
-        ("cookie.deviceId.secret =  ENC[GPG", None),
-        ("cookie deviceId secret.", None),
+        (
+            'cookie.deviceId.secret:"helloiamasecret"',
+            "filename.txt",
+            "cookie_deviceId_secret",
+        ),
+        (
+            'cookie.deviceId.secret="helloiamasecret"',
+            "filename.txt",
+            "cookie_deviceId_secret",
+        ),
+        (
+            'cookie.deviceId.secret :  "helloiamasecret"',
+            "filename.txt",
+            "cookie_deviceId_secret",
+        ),
+        (
+            'cookie.deviceId.secret =  "helloiamasecret"',
+            "filename.txt",
+            "cookie_deviceId_secret",
+        ),
+        ("cookie.deviceId.secret:ENC[GPG", "filename.txt", None),
+        ("cookie.deviceId.secret=ENC[GPG", "filename.txt", None),
+        ("cookie.deviceId.secret :  ENC[GPG", "filename.txt", None),
+        ("cookie.deviceId.secret =  ENC[GPG", "filename.txt", None),
+        ("cookie deviceId secret.", "filename.txt", None),
     ],
 )
-def test_cookie_deviceId_secret(test_input, expected):
-    assert secrets_filecontent.detect_secret_in_line(test_input) == expected
+def test_cookie_deviceId_secret(line, file, expected):
+    assert secrets_filecontent.detect_secret_in_line(line, file) == expected
 
 
 # sso_encryption_key
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "line, file, expected",
     [
-        ('sso.encryption.key:"P5xsJ9Nt+quxDKzB4DeLfw=="', "sso_encryption_key"),
-        ("sso.encryption.key=P5xsJ9Nt+quxDKzB4DeLfw==", "sso_encryption_key"),
-        ("sso.encryption.key :  P5xsJ9Nt+quxDKzB4DeLfw==", "sso_encryption_key"),
-        ("sso.encryption.key =  P5xsJ9Nt+quxDKzB4DeLfw==", "sso_encryption_key"),
-        ("sso.encryption.key:ENC[GPG", None),
-        ("sso.encryption.key=ENC[GPG", None),
-        ("sso.encryption.key :  ENC[GPG", None),
-        ("sso.encryption.key =  ENC[GPG", None),
-        ("sso encryption key.", None),
+        (
+            'sso.encryption.key:"P5xsJ9Nt+quxDKzB4DeLfw=="',
+            "filename.txt",
+            "sso_encryption_key",
+        ),
+        (
+            "sso.encryption.key=P5xsJ9Nt+quxDKzB4DeLfw==",
+            "filename.txt",
+            "sso_encryption_key",
+        ),
+        (
+            "sso.encryption.key :  P5xsJ9Nt+quxDKzB4DeLfw==",
+            "filename.txt",
+            "sso_encryption_key",
+        ),
+        (
+            "sso.encryption.key =  P5xsJ9Nt+quxDKzB4DeLfw==",
+            "filename.txt",
+            "sso_encryption_key",
+        ),
+        ("sso.encryption.key:ENC[GPG", "filename.txt", None),
+        ("sso.encryption.key=ENC[GPG", "filename.txt", None),
+        ("sso.encryption.key :  ENC[GPG", "filename.txt", None),
+        ("sso.encryption.key =  ENC[GPG", "filename.txt", None),
+        ("sso encryption key.", "filename.txt", None),
     ],
 )
-def test_sso_encrpytion_key(test_input, expected):
-    assert secrets_filecontent.detect_secret_in_line(test_input) == expected
+def test_sso_encrpytion_key(line, file, expected):
+    assert secrets_filecontent.detect_secret_in_line(line, file) == expected
