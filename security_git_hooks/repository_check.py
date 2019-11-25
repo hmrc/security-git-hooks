ab0924b190271652da8a1548c02cebac7312627f
ChristyHD <44365312+ChristyHD@users.noreply.github.com>
2019-11-25 12:02:28 +0000

    syntax

--- security_git_hooks/repository_check.py
+++ security_git_hooks/repository_check.py
@@ -3,12 +3,14 @@
 import os
 
 def repository_check():
-    if os.path.exists('./repository.yaml') is True
+    if os.path.exists('./repository.yaml') is True:
         return 0
     else:
         print("No repository.yaml found")
         return 1
 
-if __name__ == __main__:
-    exit(repository_check())
-    exit(repository_check())
+def main():
+    repository_check()
+
+if __name__ == "__main__":
+    exit(main())
